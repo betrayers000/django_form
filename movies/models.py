6dd15564a365834b8f9264defa54e9e98f068896
jw <betrayers000@gmail.com>
2019-10-28 17:19:14 +0900

    form repush

---
+++ movies/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    title_en = models.CharField(max_length=100)
+    audience = models.IntegerField()
+    open_date = models.DateField()
+    genre = models.CharField(max_length=100)
+    watch_grade = models.CharField(max_length=100)
+    score = models.FloatField()
+    poster_url = models.TextField()
+    description = models.TextField()
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-id',)
+        # 만들때 순서를 지정할수있다.
